725ee89d1a4e793f77f1d7e8b9d7a44b7bc85c3e
Adam De Fouw <aldefouw@medicine.wisc.edu>
2024-02-15 15:38:22 -0600

    Update download_repos.sh

--- download_repos.sh
+++ download_repos.sh
@@ -1,22 +1,7 @@
 #!/usr/bin/env bash
 
-git submodule init
-
-# Define submodule URLs and paths
-declare -a submodule_urls=("git@github.com:aldefouw/redcap_cypress.git" "git@github.com:aldefouw/redcap_docker.git")
-declare -a submodule_paths=("redcap_cypress" "redcap_docker")
-
-# Clone submodule repositories and add them as submodules
-for ((i=0; i<${#submodule_urls[@]}; ++i)); do
-    submodule_url=${submodule_urls[$i]}
-    submodule_path=${submodule_paths[$i]}
-
-    # Clone submodule repository
-    git clone $submodule_url $submodule_path
-
-    # Add submodule to main repository
-    git submodule add $submodule_url $submodule_path
-done
+git submodule update --init --recursive
+git submodule update --recursive --remote
 
 #Move Base Configurations for Cypress
 cp ./config/cypress.config.js ./redcap_cypress/cypress.config.js
