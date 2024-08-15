65bf20ad86157644781587031556c736f3d03832
Adam De Fouw <aldefouw@medicine.wisc.edu>
2024-08-15 11:16:08 -0500

    Update download_redcap.sh

--- download_redcap.sh
+++ download_redcap.sh
@@ -1,5 +1,91 @@
 #!/usr/bin/env bash
 
+check_zip_integrity() {
+    local file=$1
+    unzip -t "$file" &> /dev/null
+    return $? # Return the exit status of the unzip command
+}
+
+attempt_unzip_redcap() {
+    local zip_file=$1
+
+    if check_zip_integrity "${zip_file}"; then
+
+        #Target Directory
+        target_directory=./tmp
+
+        # Define the destination directory
+        destination_directory="redcap_source/redcap_v${redcap_version}"
+
+        # Check if the curl request (or zip exists)
+        if [ -e "$zip_file" ]; then
+
+            # Unzip the file to the target directory
+            unzip -q "${zip_file}" -d ${target_directory}
+
+            if [ $? -eq 0 ]; then
+
+                echo "Files for REDCap v${redcap_version} downloaded and unzipped successfully in ${target_directory}."
+
+                redcap_source_dir="./redcap_source"
+
+                #If we are upgrading
+                if [ -e "$redcap_source_dir" ]; then
+
+                  #Empty destination (what we want)
+                  if [ -z "$(ls -A "$destination_directory")" ]; then
+
+                      #We are just copying the specific folder we need - not the whole install!
+                      mv ${target_directory}/redcap/redcap_v${redcap_version} ${destination_directory}
+                      if [ $? -eq 0 ]; then
+                        echo "REDCap v${redcap_version} files moved to ${destination_directory} directory."
+                      fi
+
+                  #Destination already has something ... we cannot move the files
+                  else
+                      echo "Destination directory ${destination_directory} is NOT empty.  Cannot move the REDCap v${redcap_version} files there."
+                  fi
+
+                #If this is a first time install
+                else
+
+                  #This copies the entire install
+                  mv ${target_directory}/redcap redcap_source
+                  if [ $? -eq 0 ]; then
+                    echo "REDCap full install files moved to ./redcap_source directory."
+                  fi
+
+                  #This database.php is specifically configured for this Docker container DB setup
+                  cp ./config/database.php ./redcap_source/database.php
+                  if [ $? -eq 0 ]; then
+                    echo "Database connection successfully configured."
+                  fi
+                fi
+
+                #Let's clean up the temp files
+                rm -r tmp
+                if [ $? -eq 0 ]; then
+                  echo "Cleaned up temporary files."
+                fi
+
+            else
+                echo "Failed to unzip the file."
+            fi
+
+        else
+            echo "Failed to download REDCap. Please check your credentials and try again."
+        fi
+
+    else
+        echo "No valid REDCap zip found. Please verify your REDCap Community credentials and try again."
+
+        #Delete the invalid file since it would prevent a new download in the future
+        rm -f ${zip_file}
+
+        exit 1
+    fi
+}
+
 ## What version of REDCap
 read -p "Enter REDCap version you want to install: " redcap_version
 
@@ -9,6 +95,7 @@ zip_file="./redcap${redcap_version}.zip"
 # Check if the file exists
 if [ -e "$zip_file" ]; then
     echo "Zip file for REDCap ${redcap_version} already exists.  We'll use this to zip to install."
+    attempt_unzip_redcap "${zip_file}"
 else
     echo "To download REDCap, you must login to REDCap Community site (https://redcap.vanderbilt.edu/community/) to verify you have valid license."
     echo "This script handles that login process for you."
@@ -28,60 +115,6 @@ else
     curl -o ${zip_file} -d "username=${username}&password=${password}&version=${redcap_version}&install=1" -X POST ${url}
 
     if [ $? -eq 0 ]; then
-      echo "Zip file downloaded successfully."
+        attempt_unzip_redcap "${zip_file}"
     fi
-fi
-
-#Target Directory
-target_directory=./tmp
-
-# Check if the curl request (or zip exists)
-if [ -e "$zip_file" ]; then
-
-    # Unzip the file to the target directory
-    unzip -q "${zip_file}" -d ${target_directory}
-
-    if [ $? -eq 0 ]; then
-
-        echo "Files for REDCap v${redcap_version} downloaded and unzipped successfully in ${target_directory}."
-
-        redcap_source_dir="./redcap_source"
-
-        #If we are upgrading
-        if [ -e "$redcap_source_dir" ]; then
-
-            #We are just copying the specific folder we need - not the whole install!
-            mv ${target_directory}/redcap/redcap_v${redcap_version} redcap_source/redcap_v${redcap_version}
-            if [ $? -eq 0 ]; then
-              echo "REDCap full install files moved to ./redcap_source directory."
-            fi
-
-        #If this is a first time install
-        else
-
-          #This copies the entire install
-          mv ${target_directory}/redcap redcap_source
-          if [ $? -eq 0 ]; then
-            echo "REDCap full install files moved to ./redcap_source directory."
-          fi
-
-          #This database.php is specifically configured for this Docker container DB setup
-          cp ./config/database.php ./redcap_source/database.php
-          if [ $? -eq 0 ]; then
-            echo "Database connection successfully configured."
-          fi
-        fi
-
-        #Let's clean up the temp files
-        rm -r tmp
-        if [ $? -eq 0 ]; then
-          echo "Cleaned up temporary files."
-        fi
-
-    else
-        echo "Failed to unzip the file."
-    fi
-
-else
-    echo "Failed to download REDCap. Please check your credentials and try again."
 fi
